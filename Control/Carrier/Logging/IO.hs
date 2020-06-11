658f4c5bdc451fe7a6e8e0b507bfac27d4cb0590
Rob Rix <rob.rix@me.com>
2020-06-11 19:41:43 -0400

    Define a carrier logging in IO.

--- Control/Carrier/Logging/IO.hs
+++ Control/Carrier/Logging/IO.hs
@@ -1,7 +1,31 @@
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE UndecidableInstances       #-}
 module Control.Carrier.Logging.IO
 ( module Control.Carrier.Logging.IO
 , module Control.Effect.Logging
-)
-where
+) where
 
-import Control.Effect.Logging
+import           Control.Algebra
+import           Control.Carrier.Reader
+import           Control.Effect.Logging
+import           Control.Monad.IO.Class
+import           Data.List              (intercalate)
+
+newtype LoggingT m a = LoggingT { runLogging :: ReaderC [String] m a }
+  deriving (Applicative, Functor, Monad, MonadIO)
+
+instance (Algebra sig m, MonadIO m) => Algebra (Logging :+: sig) (LoggingT m) where
+  alg (L (Log lvl msg  k)) = do
+    ctx <- LoggingT $ asks displayContext
+    liftIO $ putStrLn $ displayLevel lvl <> ": " <> "(" <> ctx <> ")" <> msg
+    k
+  alg (L (Label name m k)) = do
+    res <- LoggingT $ local (name:) (runLogging m)
+    k res
+  alg (R other)            = LoggingT (alg (R (handleCoercible other)))
+
+displayContext :: [String] -> String
+displayContext = intercalate "."
