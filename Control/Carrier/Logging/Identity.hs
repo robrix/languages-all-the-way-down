d95c7cf59454d2b6f602c6f49230fe0a69ec60cf
Rob Rix <rob.rix@me.com>
2020-06-11 12:06:58 -0400

    Define a logging carrier and algebra.

--- Control/Carrier/Logging/Identity.hs
+++ Control/Carrier/Logging/Identity.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE UndecidableInstances       #-}
 module Control.Carrier.Logging.Identity
 ( module Control.Carrier.Logging.Identity
 , module Control.Effect.Logging
 ) where
 
-import Control.Effect.Logging
+import           Control.Algebra
+import           Control.Effect.Logging
+
+newtype LoggingT m a = LoggingT { runLogging :: m a }
+  deriving (Applicative, Functor, Monad)
+
+instance Algebra sig m => Algebra (Logging :+: sig) (LoggingT m) where
+  alg (L (Log _ _   k)) = k
+  alg (L (Label _ m k)) = m >>= k
+  alg (R other)         = LoggingT (alg (handleCoercible other))
