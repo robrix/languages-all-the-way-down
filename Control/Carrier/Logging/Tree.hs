81e8b35b87b537395abcdffb68d1e078477ad8ff
Rob Rix <rob.rix@me.com>
2020-06-11 23:38:13 -0400

    Define a smart handler for LoggingT.

--- Control/Carrier/Logging/Tree.hs
+++ Control/Carrier/Logging/Tree.hs
@@ -14,15 +14,20 @@ import           Control.Carrier.Writer.Strict
 import           Control.Effect.Logging
 import           Control.Monad.IO.Class
 
-newtype LoggingT m a = LoggingT { runLogging :: WriterC [Message] (WriterC [Span] m) a }
+runLogging :: Monad m => LoggingT m a -> m (Span, a)
+runLogging (LoggingT m) = do
+  (children, (messages, a)) <- runWriter (runWriter m)
+  return (Span "" messages children, a)
+
+newtype LoggingT m a = LoggingT (WriterC [Message] (WriterC [Span] m) a)
   deriving (Applicative, Functor, Monad, MonadIO)
 
 instance (Algebra sig m, Effect sig, MonadIO m) => Algebra (Logging :+: sig) (LoggingT m) where
   alg (L (Log lvl msg  k)) = do
     LoggingT $ tell [ Message lvl msg ]
     k
-  alg (L (Label name m k)) = do
-    (messages, (children, res)) <- LoggingT $ censor @[Message] (const []) (listen (censor @[Span] (const []) (listen (runLogging m))))
+  alg (L (Label name (LoggingT m) k)) = do
+    (messages, (children, res)) <- LoggingT $ censor @[Message] (const []) (listen (censor @[Span] (const []) (listen m)))
     LoggingT $ tell [ Span name messages children ]
     k res
   alg (R other)            = LoggingT (alg (R (R (handleCoercible other))))
